d89292612127175ea4945eb33209ec73550bbd9a
Perry Sittser <sittserp@gmail.com>
2020-10-05 14:15:23 -0700

    bakery inventory

---
+++ bakery.js
@@ -0,0 +1,50 @@
+export const bakery = [
+    {
+        id: 'cookie',
+        name: 'cookie',
+        image: 'assets.chocolate-chip.jpeg',
+        description: 'Chocolate Chip Cookie',
+        category: 'chocolate',
+        price: 2
+    },
+    {
+        id: 'pie',
+        name: 'marion-berry-cream',
+        image: 'assets.pie.png',
+        description: 'Marion Berry Cream Pie',
+        category: 'berry',
+        price: 3.5
+    },
+    {
+        id: 'cupcake',
+        name: 'cupcake',
+        image: 'assets.cupcake.jpg',
+        description: 'Cupcake',
+        category: 'chocolate',
+        price: 2
+    },
+    {
+        id: 'cake',
+        name: 'chocolate-cake',
+        image: 'chocolate-cake.jpg',
+        description: 'Double Chocolate Cake',
+        category: 'chocolate',
+        price: 3.5
+    },
+    {
+        id: 'scone',
+        name: 'blueberry-scone',
+        image: 'blueberry-scone.jpg',
+        description: 'Blueberry Scone',
+        category: 'berry',
+        price: 2.5
+    },
+    {
+        id: 'cinnamon-roll',
+        name: 'cinnamon-roll',
+        image: 'cinnamon-rolls.png',
+        description: 'Cinnamon Roll',
+        category: 'cinnamon',
+        price: 3
+    },
+];
