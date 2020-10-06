0acd064f34298f4c5c1a2fe872154a1c585cdf57
Perry Sittser <sittserp@gmail.com>
2020-10-06 15:08:17 -0700

    TDD findById Function

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderBakery } from '../utils.js';
+import { renderBakery, findById } from '../utils.js';
+import { bakery } from '../bakery.js'
 
 const test = QUnit.test;
 
@@ -27,4 +28,29 @@ test('I expect the function to render the same as the hard-coded example.', (exp
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
+});
+
+test('I expect the function to takes an array and an id, and returns the first item found that has an .id property that matches the passed in id.', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+
+    const someArray = bakery;
+    const someID = 'cupcake';
+
+    const expected = {
+        id: 'cupcake',
+        name: 'Cupcake',
+        image: '../assets/cupcake.png',
+        description: 'Chocolate frosting on yellow cake',
+        category: 'chocolate',
+        price: 2
+    };
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = findById(someArray, someID);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
 });
--- utils.js
+++ utils.js
@@ -26,3 +26,12 @@ export function renderBakery(bakedItem) {
     return li;
     
 }
+
+export function findById(someArray, someId) {
+    for (let i = 0; i < someArray.length; i++) {
+        const myID = someArray[i];
+        if (myID.id === someId) {
+            return myID;
+        }
+    }
+}
