96525338f5119704cdafc320c5827d0f756dcc34
Perry Sittser <sittserp@gmail.com>
2020-10-08 18:04:36 -0700

    added admin link to all pages

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderBakery, findById, calcLineItem, renderCart, calcOrderTotal, addProduct } from '../utils.js';
-import { bakery } from '../bakery.js';
-import { cartItems } from '../cart/cart-items.js';
+import { renderBakery, findById, calcLineItem, renderCart, calcOrderTotal, addProduct, getLocalProducts } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -31,11 +29,11 @@ test('I expect the function to render the same as the hard-coded example.', (exp
     expect.equal(actual.outerHTML, expected);
 });
 
-test('I expect the function to take an array and an id, and returns the first item found that has an .id property that matches the passed in id.', (expect) => {
+test('I expect the function to take an array and an id, and return the first item found that has an .id property that matches the passed in id.', (expect) => {
     //Arrange
     // Set up your arguments and expectations
 
-    const someArray = bakery;
+    const someArray = getLocalProducts();
     const someID = 'cupcake';
 
     const expected = {
@@ -136,64 +134,7 @@ test('ADD PRODUCT: I expect the function to take an object and inject it into lo
         price: 3
     };
 
-    const expected = [
-        {
-            'category': 'chocolate',
-            'description': 'Thin, Crisp, and Crunchy with no apologies',
-            'id': 'cookie',
-            'image': '../assets/chocolate-chip.png',
-            'name': 'Chocolate Chip Cookie',
-            'price': 2
-        },
-        {
-            'category': 'berry',
-            'description': 'Delicious berry pie filling over our cream cheese cream pie base',
-            'id': 'pie',
-            'image': '../assets/pie.jpg',
-            'name': 'Marion Berry Cream Pie Slice',
-            'price': 3.5
-        },
-        {
-            'category': 'chocolate',
-            'description': 'Chocolate frosting on yellow cake',
-            'id': 'cupcake',
-            'image': '../assets/cupcake.png',
-            'name': 'Cupcake',
-            'price': 2
-        },
-        {
-            'category': 'chocolate',
-            'description': 'We double down on double chocolate',
-            'id': 'cake',
-            'image': '../assets/chocolate-cake.png',
-            'name': 'Chocolate Cake Slice',
-            'price': 3.5
-        },
-        {
-            'category': 'berry',
-            'description': 'Solid and Flaky - NOT a muffin...',
-            'id': 'scone',
-            'image': '../assets/blueberry-scone.png',
-            'name': 'Blueberry Scone',
-            'price': 2.5
-        },
-        {
-            'category': 'cinnamon',
-            'description': 'Extra Cinnamon and Extra Roll',
-            'id': 'cinnamon-roll',
-            'image': '../assets/cinnamon-rolls.png',
-            'name': 'Cinnamon Roll',
-            'price': 3
-        },
-        {
-            'category': 'chocolate',
-            'description': 'All the yummy goodness',
-            'id': 'eclair',
-            'image': '../assets/eclair.png',
-            'name': 'Éclair',
-            'price': 3
-        }
-    ];
+    const expected = getLocalProducts().concat(newProduct);
     
     //Act 
     // Call the function you're testing and set the result to a const
